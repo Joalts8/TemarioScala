b618b250946225bdbb322035e691579db7e8aaeb
Joalts8 <jorgenexus8@gmail.com>
2025-03-03 13:34:12 +0100

    Funcion de edit de funcion

--- src/main/scala/Funcional.scala
+++ src/main/scala/Funcional.scala
@@ -1,6 +1,6 @@
 object Funcional extends App{
   //Pueden existir funciones en funciones
-  //funcion polimorfica-> para tipo generico usando [T]
+  //funcion polimorfica-> para tipo generico usando [T], y usando funcion A-> bollean
   def busca[A](v: Array[A], p: A => Boolean) = {  // esta condicion se puede definir _condicion(_ es generico) o elem=> condicion
     def bucle(n: Int): Int =
       if (n >= v.length) -1
@@ -9,6 +9,10 @@ object Funcional extends App{
 
     bucle(0)
   }
+
+  // edit de una funcion, en este caso, dado A y f(A,B)=C, devuelve g(B)=C
+  def partial1[A,B,C](a:A,f:(A,B)=>C):B => C =
+    y => f(a,y)
 
   //ejemplo sintasis en funcion polimorfica
   val array= Array[Int](1,2,3,4,5)
