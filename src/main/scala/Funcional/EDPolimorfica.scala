a157bfed03d8541975831608e1fa221024539d88
Joalts8 <jorgenexus8@gmail.com>
2025-03-05 17:59:41 +0100

    Adicion de explicacion de metodosde foldR y foldL

--- src/main/scala/Funcional/EDPolimorfica.scala
+++ src/main/scala/Funcional/EDPolimorfica.scala
@@ -19,7 +19,6 @@ enum EDPolimorfica[+A]:
     }
     recursion(l, 0)
   }
-
   //funcion para operacion cualquiera definida con f(x, y)(En funciones polinomicas ejemplo de sintais); recursividad de cola
   def operar[A](l: EDPolimorfica[A], inic: A, f: (A, A) => A) = {
     def bucle(l: EDPolimorfica[A], acc: A): A = {
@@ -29,36 +28,34 @@ enum EDPolimorfica[+A]:
     }
     bucle(l, inic)
   }
-
-
-
+  //ejecucion de una funcion en distinto orden, la 1a de derecha a izquierda;   se puede aplicar a operar tambien
   def foldRight[A, B](l: EDPolimorfica[A], acc: B, f: (A, B) => B): B = {
     l match
       case Nula => acc
       case Cons(a, r) => f(a, foldRight(r, acc, f))
-  } //resuelve de derecha a izda
+  }
+  //resuelve de izquierdsa a derecha(como operar y anteriores por cola)
   def foldLeft[A, B](l: EDPolimorfica[A], acc: B, f: (A, B) => B): B = {
     l match
       case Nula => acc
-      case Cons(a, r) => foldLeft(r, f(a, acc), f) //acumula al revés
+      case Cons(a, r) => foldLeft(r, f(a, acc), f)
   }
 
+  //ejemplo del uso de foldRigth
   def longitud[A](l: EDPolimorfica[A]) = foldRight[A, Int](l, 0, (x, y) => y + 1)
-
+  //Uso para crear mapa donde Key son los valores de EDP y Values los resultados de f(k)
   def map[A, B](l: EDPolimorfica[A], f: A => B): EDPolimorfica[B] = {
     l match
       case Nula => Nula
       case Cons(a, r) => Cons(f(a), map(r, f))
   }
   def map1[A, B](l: EDPolimorfica[A], f: A => B): EDPolimorfica[B] =foldRight[A, EDPolimorfica[B]](l, Nula, (x, y) => Cons(f(x), y))
-
-
+  //creacion de un flitro, funcion que devuelbe boolean
   def filter[A](l: EDPolimorfica[A], f: A => Boolean): EDPolimorfica[A] = {
     l match
       case Nula => Nula
       case Cons(a, r) => if (f(a)) Cons(a, filter(r, f)) else filter(r, f)
   }
-
   def filter1[A](l: EDPolimorfica[A], f: A => Boolean): EDPolimorfica[A] = foldRight[A, EDPolimorfica[A]](l, Nula, (x, y) => if (f(x)) Cons(x, y) else y)
 
 
@@ -68,4 +65,8 @@ object EDPolimorfica {
   def apply[A](args: A*): EDPolimorfica[A] =
     if (args.isEmpty) Nula
     else Cons(args(0), apply(args.tail *))
+}
+
+object pruebas extends App {
+
 }
