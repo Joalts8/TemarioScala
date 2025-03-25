17d5038b40f2f627902f39664b30187481a14112
Joalts8 <jorgenexus8@gmail.com>
2025-03-25 10:16:04 +0100

    Hebras

--- src/main/scala/Concurrente/Introduccion/Hebra.scala
+++ src/main/scala/Concurrente/Introduccion/Hebra.scala
@@ -1,16 +1,24 @@
 package Concurrente.Introduccion
-//HASTA PG 23; falta sleep y join
+
+//Proceso ligero-> Hebra; run es lo que se ejecuta al invocarla(Hilo de to la vida)
 class Hebra extends Thread{
  override def run()=println("Hola Mundo")
 }
-
+//Hebra implementando la interfaz Runnable-> da metodo run
 class Escribir(c: Char) extends Runnable {
  override def run =
   for (i <- 0 until 10) print(c)
 }
 
 object mainHebra extends App{
+ //Creacion de Hebras heredando Thread e implmentando Runnable
  val h1= new Hebra
- val h2 = new Thread(new Escribir('A'))
- h1.start(); h2.start()
+ val h2 = new Thread(new Escribir('A')) //se pasa su referencia a un Thread
+ val h4 = new Thread(new Escribir('B'))
+ val h3 = new Thread(new Escribir('C'))
+ //Metodos de las Hebras
+ h1.start(); h2.start();h4.start();h3.start()    //Ejecuta asíncrona, NUNCA RUN=> secuencial
+ h4.join()    //Metodo de sincronización, espera a que h4 acabe
+ Thread.sleep(5000)   //suspender durante 5 seg
+ println(Thread.currentThread().getName)   //Devuelve el nombre(puede h.getName) la hebra que se está ejecutando(existe h.setName)
 }
