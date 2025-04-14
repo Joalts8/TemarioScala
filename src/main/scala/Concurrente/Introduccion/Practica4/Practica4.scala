fbb3718a5d9eaf56d98bf283c801bd0c2507043f
Joalts8 <jorgenexus8@gmail.com>
2025-04-14 14:34:35 +0200

    P4.1

--- src/main/scala/Concurrente/Introduccion/Practica4/Practica4.scala
+++ src/main/scala/Concurrente/Introduccion/Practica4/Practica4.scala
@@ -1,6 +1,7 @@
 package Concurrente.Introduccion.Practica4
 
-@volatile val turno = 0
+@volatile var turno = 0
+@volatile var iter=0
 class EJ1(c:Char, t:Int) extends Thread{
   override def run()={
     for(i<-0 until t){
@@ -9,19 +10,33 @@ class EJ1(c:Char, t:Int) extends Thread{
   }
 }
 class EJ1b(id:Int, c: Char, t: Int) extends Thread {
-  //turno e iter?
   override def run() = {
     for (i <- 0 until t) {
       while(turno!= id) Thread.sleep(0)
       print(c)
+      iter=iter+1
+      if((id+1)==iter){
+        iter=0;turno=(turno+1)%3
+      }
     }
   }
 }
 
-object salida extends App{
- 
+def periodico(t: Long)(b: => Unit): Thread={
+
+}
+
+
+@main def EJ1Main = {
   val hilo1=new EJ1('A',3)
-  val hilo2=new EJ1('B', 5)
+  val hilo2=new EJ1('B',5)
   val hilo3=new EJ1('C',9)
   hilo1.start();hilo2.start();hilo3.start()
+}
+@main def EJ1bMain = {
+  val hilo1b = new EJ1b(0,'A', 3)
+  val hilo2b = new EJ1b(1,'B', 6)
+  val hilo3b = new EJ1b(2,'C', 9)
+  hilo1b.start();hilo2b.start();hilo3b.start()
+
 }
