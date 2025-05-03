925188d5a7abe8f53b8b1165f3e306ea00f18e92
Joalts8 <jorgenexus8@gmail.com>
2025-05-03 14:51:31 +0200

    P6.8 Concluí

--- src/main/scala/Concurrente/Semaforos/Practica6/Ejercicio8.scala
+++ src/main/scala/Concurrente/Semaforos/Practica6/Ejercicio8.scala
@@ -4,27 +4,32 @@ import java.util.concurrent.*
 import scala.util.Random
 
 class Olla(R: Int) {
+  private val mutex = new Semaphore(1) //Exclusion mutua
+  private val canibal = new Semaphore(1)
+  private val cocinero = new Semaphore(0)
   // CS-caníbal i: no coge una ración de la olla si está vacía
   // CS-cocinero: no cocina un nuevo explorador hasta que la olla está vacía
   private var olla = R // inicialmente llena
-  // ...
 
   def racion(i: Int) = {
-    // caníbal i coge una ración de la olla
-    // ...
+    canibal.acquire()
+    mutex.acquire()
+    olla-=1
     log(s"Caníbal $i coge una ración de la olla. Quedan $olla raciones.")
-    // ...
+    if (olla>0) canibal.release() else cocinero.release()
+    mutex.release()
   }
 
   def dormir = {
-    // cocinero espera a que la olla esté vacía
-    // ...
+    cocinero.acquire()
   }
 
   def llenarOlla = {
-    // ...
+    mutex.acquire()
+    olla=R
     log(s"El cocinero llena la olla. Quedan $olla raciones.")
-    // ...
+    canibal.release()
+    mutex.release()
   }
 }
 
