0517a97da846a124737aa1bbc1c654d48b409498
Joalts8 <jorgenexus8@gmail.com>
2025-05-03 13:41:38 +0200

    P6.4

--- src/main/scala/Concurrente/Semaforos/Practica6/Ejercicio4.scala
+++ src/main/scala/Concurrente/Semaforos/Practica6/Ejercicio4.scala
@@ -4,34 +4,36 @@ import java.util.concurrent.*
 import scala.util.Random
 
 class Coche(C: Int) extends Thread {
-  // CS-pasajero1: si el coche está lleno, un pasajero no puede subir al coche hasta que haya terminado
-  // el viaje y se hayan bajado los pasajeros de la vuelta actual
-  // CS-pasajero2: un pasajero que está en el coche no puede bajarse hasta que haya terminado el viaje
-  // CS-coche: el coche espera a que se hayan subido C pasajeros para dar una vuelta
+  val pasajeroSubir= new Semaphore(1)
+  val pasajeroBajar= new Semaphore(0)
+  val coche = new Semaphore(0)
+  val mutex = new Semaphore(1)
   private var numPas = 0
-  // ...
 
   def nuevoPaseo(id: Int) = {
-    // el pasajero id quiere dar un paseo en la montaña rusa
-    // ...
+    pasajeroSubir.acquire()
+    mutex.acquire()
+    numPas+=1
     log(s"El pasajero $id se sube al coche. Hay $numPas pasajeros.")
-    // ...
+    if(numPas<C) pasajeroSubir.release() else coche.release()
+    mutex.release()
+
+    pasajeroBajar.acquire()
+    mutex.acquire()
+    numPas-=1
     log(s"El pasajero $id se baja del coche. Hay $numPas pasajeros.")
-    // ...
+    if(numPas>0) pasajeroBajar.release() else pasajeroSubir.release()
+    mutex.release()
   }
 
   def esperaLleno = {
-    // el coche espera a que se llene para dar un paseo
-    // ...
+    coche.acquire()
     log(s"        Coche lleno!!! empieza el viaje....")
-    // ...
   }
 
   def finViaje = {
-    // el coche indica que se ha terminado el viaje
-    // ...
     log(s"        Fin del viaje... :-(")
-    // ...
+    pasajeroBajar.release()
   }
 
   override def run = {
