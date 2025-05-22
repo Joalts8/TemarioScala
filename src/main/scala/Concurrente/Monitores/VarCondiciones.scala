22941f847ca58972dde815bf47ddc847148793fa
Joalts8 <jorgenexus8@gmail.com>
2025-05-22 13:17:19 +0200

    VarCond

--- src/main/scala/Concurrente/Monitores/VarCondiciones.scala
+++ src/main/scala/Concurrente/Monitores/VarCondiciones.scala
@@ -1,17 +1,17 @@
 package Concurrente.Monitores
 import Concurrente.Introduccion.*
 
+//Sirve para usar distintos monitores en el objeto-> más parecido a semaforo
 class Condition {
   def cwait() = synchronized {
     wait()
   }
-  def cnotify() = synchronized {
-    notify()
-  }
-  def cnotifyAll = synchronized { notifyAll() }
+  def cnotify() = synchronized {notify()}//En este metodo lo pongo en la misma línea porq solo hace 1 cosa
+  def cnotifyAll = synchronized {notifyAll()}//En este metodo, no usa ()=> al llamarlo no hcen falta
 }
 
-object gestorBDCondiciones1 {
+//Ejemplo con P-C, pero se puede Blockear=> Usar Locks
+object productorConsumidor {
   private var nLectores = 0
   private var escribiendo = false
   private var nEscritores = 0
