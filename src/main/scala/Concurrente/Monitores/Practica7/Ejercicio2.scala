f1a09468092fca209803984056dc1da3712d96cf
Joalts8 <jorgenexus8@gmail.com>
2025-05-25 13:33:28 +0200

    P7.

--- src/main/scala/Concurrente/Monitores/Practica7/Ejercicio2.scala
+++ src/main/scala/Concurrente/Monitores/Practica7/Ejercicio2.scala
@@ -2,27 +2,42 @@ package Concurrente.Monitores.Practica7
 
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
+import java .util.concurrent.locks.*
 
 class Recursos(rec:Int) {
-
-  
+  private val l= new ReentrantLock(true)
   private var numRec = rec
+  private var procesos=0
+  private val cBlock=l.newCondition()
+  private var list=new ListBuffer[Int]
   
   def pidoRecursos(id:Int,num:Int) =  {
-    //proceso id solicita num recursos
-      
-      log(s"Proceso $id pide $num recursos.")
-      
-    
+    l.lock()
+    try {
+      procesos+=1
+      list.append(id)
+      log(s"Proceso $id pide $num recursos. Esperando $procesos")
+      while(list(0)!=id || num>numRec) cBlock.await()
+      numRec-=num
+      list=list.tail
+      procesos-=1
       log(s"Proceso $id coge $num recursos. Quedan $numRec")
-      
+    }finally{
+      l.unlock()
+    }
   }
 
   def libRecursos(id:Int,num:Int) =  {
     //proceso id devuelve num recursos
-   
-    log(s"Proceso $id devuelve $num recursos. Quedan $numRec")
-    
+    l.lock()
+    try {
+      numRec+=num
+      log(s"Proceso $id devuelve $num recursos. Quedan $numRec")
+    } finally {
+      cBlock.signalAll()
+      l.unlock()
+    }
+
   }
 }
 object Ejercicio2 {
@@ -34,12 +49,12 @@ object Ejercicio2 {
     val proceso = new Array[Thread](numProc)
     for (i<-proceso.indices)
       proceso(i) = thread{
-      //  while (true){
+        while (true){
           val r = Random.nextInt(rec)+1
           recursos.pidoRecursos(i,r)
           Thread.sleep(Random.nextInt(300))
           recursos.libRecursos(i,r)
-     //   }
+        }
       }
   }
 }
