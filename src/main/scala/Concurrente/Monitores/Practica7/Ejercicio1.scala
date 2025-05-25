78acb4e1fc8f174424c12286c207130ba77732a5
Joalts8 <jorgenexus8@gmail.com>
2025-05-25 13:04:21 +0200

    P7.1

--- src/main/scala/Concurrente/Monitores/Practica7/Ejercicio1.scala
+++ src/main/scala/Concurrente/Monitores/Practica7/Ejercicio1.scala
@@ -1,24 +1,53 @@
 package Concurrente.Monitores.Practica7
 
 import scala.util.Random
+import java.util.concurrent.locks.*
 
 class Buffer(ncons:Int,tam:Int){
   //ncons-número de consumidores
   //tam-tamaño del buffer
+  private val l = new ReentrantLock(true)
+  private val cVacio= l.newCondition()
+  private val cLleno=l.newCondition()
+  private var n=0
+  private var i=0
+  private val j = Array.fill(ncons)(0)
+  private val consumido = Array.fill(tam)(0)
   private val buffer = new Array[Int](tam)
   
   def nuevoDato(dato:Int) = {
-    //el productor pone un nuevo dato
-    
-   
-    log(s"Productor almacena $dato: buffer=${buffer.mkString("[",",","]")}}")
-    
+    l.lock()
+    try {
+      while(n==tam) cLleno.await()
+      n+=1
+      buffer(i) = dato
+      i = (i + 1) % tam
+      log(s"Productor almacena $dato: buffer=${buffer.mkString("[", ",", "]")}}")
+    }finally {
+      cVacio.signalAll()
+      l.unlock()
+    }
   }
 
   def extraerDato(id:Int):Int =  {
-    
-    log(s"Consumidor $id lee : buffer=${buffer.mkString("[",",","]")}")
-    0
+    l.lock()
+    try {
+      while (n == 0) cVacio.await()
+      val aux = buffer(j(id))
+      if(consumido(j(id))==ncons-1){
+        buffer(j(id)) = 0
+        consumido(j(id))=0
+        n -= 1
+        cLleno.signal()
+      }else{
+        consumido(j(id))+=1
+      }
+      j(id) = (j(id) + 1) % tam
+      log(s"Consumidor $id lee:$aux buffer=${buffer.mkString("[", ",", "]")}")
+      aux
+    }finally {
+      l.unlock()
+    }
   }
 }
 object Ejercicio1 {
