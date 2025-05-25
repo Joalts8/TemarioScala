c9198c0748057aaa6ed92e44ececeb3d056b27b9
Joalts8 <jorgenexus8@gmail.com>
2025-05-25 14:24:03 +0200

    P7.2 y 7.3

--- src/main/scala/Concurrente/Monitores/Practica7/Ejercicio3.scala
+++ src/main/scala/Concurrente/Monitores/Practica7/Ejercicio3.scala
@@ -1,19 +1,48 @@
 package Concurrente.Monitores.Practica7
 
+import java.util.concurrent.locks.ReentrantLock
 import scala.util.Random
 
 object Parejas{
-  
+  private val l= new ReentrantLock(true)
+  private var hombre=false
+  private var mujer=false
+  private val cMujer=l.newCondition()
+  private val cHombre=l.newCondition()
+  private val esperar=l.newCondition()
 
   def llegaHombre(id:Int) = {
-
-    log(s"Hombre $id quiere formar pareja")
-
+    l.lock()
+    try{
+      while(hombre) cHombre.await()
+      hombre=true
+      log(s"Hombre $id quiere formar pareja")
+      if(!mujer) esperar.await() else{
+        esperar.signalAll()
+        log(s"Se ha formado una pareja!!!")
+      }
+      hombre = false
+      cHombre.signal()
+      mujer = false
+      cMujer.signal()
+    }finally {
+      l.unlock()
+    }
   }
 
   def llegaMujer(id: Int) =  {
-   
-    log(s"Mujer $id quiere formar pareja")
+    l.lock()
+    try {
+      while (mujer) cMujer.await()
+      mujer = true
+      log(s"Mujer $id quiere formar pareja")
+      if(!hombre) esperar.await()else {
+        esperar.signalAll()
+        log(s"Se ha formado una pareja!!!")
+      }
+    } finally {
+      l.unlock()
+    }
   }
 }
 object Ejercicio3 {
