e4bbef836201715a22a4a4d45b41e60c1e03619a
Joalts8 <jorgenexus8@gmail.com>
2025-05-27 17:31:49 +0200

    P7.6 Volanding

--- src/main/scala/Concurrente/Monitores/Practica7/Ejercicio6.scala
+++ src/main/scala/Concurrente/Monitores/Practica7/Ejercicio6.scala
@@ -1,26 +1,39 @@
 package Concurrente.Monitores.Practica7
 
+import java.util.concurrent.locks.ReentrantLock
 import scala.util.Random
 
 class Bandeja(R:Int){
-
   private var raciones = 0
-  
-  
+  private val l = new ReentrantLock(true)
+  private val ctarta=l.newCondition()
+  private val cnueva=l.newCondition()
 
   def quieroRacion(id:Int)= {
-    
-    log(s"Niño $id ha cogido una ración. Quedan $raciones")
-   
+    l.lock()
+    try {
+      while(raciones==0)ctarta.await()
+      raciones-=1
+      log(s"Niño $id ha cogido una ración. Quedan $raciones")
+      if(raciones==0)cnueva.signal()
+    } finally {
+      l.unlock()
+    }
   }
   def tarta()= {
-    
-    log("El pastelero pone una nueva tarta.")
-   
+    l.lock()
+    try {
+      while (raciones>0)cnueva.await()
+      raciones=R
+      log(s"El pastelero pone una nueva tarta. Raciones= $R")
+      ctarta.signalAll()
+    } finally {
+      l.unlock()
+    }
   }
 }
-object Ejercicio6 {
 
+object Ejercicio6 {
   def main(args:Array[String]):Unit = {
     val R = 5
     val N = 10
