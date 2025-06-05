86926b49175b3a88cab420bda3abd5236a8cfe33
MontanezJM <162050835+MontanezJM@users.noreply.github.com>
2025-06-05 21:33:55 +0200

    Update anti deadlock Ejercicio4.scala Monitores
    El hecho de poner while(numPas == C) paseo.await implica que si un pasajero intenta subirse mientras los demás se bajan, detectará que numpas != C y se subirá, haciendo que el mamón se suba mientras todos se bajan (condición de carrera como una puta casa mi loko)

--- src/main/scala/Concurrente/Monitores/Practica7/Ejercicio4.scala
+++ src/main/scala/Concurrente/Monitores/Practica7/Ejercicio4.scala
@@ -1,5 +1,7 @@
 package Concurrente.Monitores.Practica7
 
+import Laboratorio7.{log, thread}
+
 import java.util.concurrent.locks.ReentrantLock
 import scala.util.Random
 
@@ -9,32 +11,36 @@ class Coche(C:Int) extends Thread{
   private val bajar= l.newCondition()
   private val paseo= l.newCondition()
   private var numPas = 0
+  private var hopOff = false
 
   def nuevoPaseo(id:Int)= {
     //el pasajero id  quiere dar un paseo en la montaña rusa
     l.lock()
     try {
-      if (numPas==C)paseo.await()
+      while (numPas==C || hopOff)paseo.await()
       numPas+=1
       log(s"El pasajero $id se sube al coche. Hay $numPas pasajeros.")
       if(numPas==C) llenar.signal()
       bajar.await()
       numPas-=1
       log(s"El pasajero $id se baja del coche. Hay $numPas pasajeros.")
-      if(numPas==0) paseo.signalAll()
+      if(numPas==0){
+        hopOff = false
+        paseo.signalAll()
+      }
     } finally {
       l.unlock()
     }
 
 
-   
+
   }
 
   def esperaLleno =  {
     //el coche espera a que se llene para dar un paseo
     l.lock()
     try {
-      if (numPas<C)llenar.await()
+      while (numPas<C)llenar.await()
       log(s"        Coche lleno!!! empieza el viaje....")
     } finally {
       l.unlock()
@@ -47,6 +53,7 @@ class Coche(C:Int) extends Thread{
     l.lock()
     try {
       log(s"        Fin del viaje... :-(")
+      hopOff = true
       bajar.signalAll()
     } finally {
       l.unlock()
@@ -62,15 +69,15 @@ class Coche(C:Int) extends Thread{
   }
 }
 object Ejercicio4 {
-  def main(args:Array[String])=
+  def main(args:Array[String])= {
     val coche = new Coche(5)
     val pasajero = new Array[Thread](20)
     coche.start()
-    for (i<-0 until pasajero.length)
-      pasajero(i) = thread{
-   //     while (true)
-          Thread.sleep(Random.nextInt(500))
-          coche.nuevoPaseo(i)
+    for (i <- 0 until pasajero.length)
+      pasajero(i) = thread {
+        //     while (true)
+        Thread.sleep(Random.nextInt(500))
+        coche.nuevoPaseo(i)
       }
-      
+  }
 }
